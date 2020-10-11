c884c3a0db8b19fb348d0443e8970e9e38e93184
cyj15857512279 <2973107605@qq.com>
2020-10-11 14:07:29 +0800

    erq

--- hello.py
+++ hello.py
@@ -1,10 +1,1 @@
-# -*- coding: utf-8 -*-
-import numpy as np
-import matplotlib.pyplot as plt
-from pylab import *
-x = np.arange(-5.0, 5.0, 0.01)
-y1 = np.sin(x)
-plt.figure(1)
-plt.subplot(211)
-plt.plot(x, y1)
-show()
+skakaks
