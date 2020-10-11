d01674aff0cf8ce6c68731255816176d29a9916d
cyj15857512279 <2973107605@qq.com>
2020-10-11 13:10:46 +0800

    tre

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-jljo
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import *
+x = np.arange(-5.0, 5.0, 0.01)
+y1 = np.sin(x)
+plt.figure(1)
+plt.subplot(211)
+plt.plot(x, y1)
+show()
