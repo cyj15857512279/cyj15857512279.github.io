42e86c750b5778238be81ac4d50520e3328e0e30
cyj15857512279 <2973107605@qq.com>
2020-10-15 09:26:58 +0800

    kol

---
+++ uy.py/阶跃函数.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pylab as plt
+def step_function1(x=''):
+    if x >0:
+        return 1
+    else:
+        return 0
+def step_function2(x):
+    y = x > 0  
+    return y.astype(np.int)  
+m = 1
+print(step_function1(m))
+x = np.arange(-4.0,4.0,0.05) 
+y = step_function2(x)
+plt.plot(x,y)
+plt.ylim(-0.1,1.1)
+plt.show()
